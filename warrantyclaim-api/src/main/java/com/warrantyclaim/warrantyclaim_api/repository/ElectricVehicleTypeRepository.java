618008ecc863c5c8edfd93b41f5ac4ea560d7a47
anselamkeykamona <sit96leminhduc@gmail.com>
2025-10-09 16:08:04 +0700

    Add Repository for ElectricVehicleType, ElectricVehicle, WarrantyClaim, WorkAssign

---
+++ warrantyclaim-api/src/main/java/com/warrantyclaim/warrantyclaim_api/repository/ElectricVehicleTypeRepository.java
@@ -0,0 +1,8 @@
+package com.warrantyclaim.warrantyclaim_api.repository;
+
+import com.warrantyclaim.warrantyclaim_api.entity.ElectricVehicleType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ElectricVehicleTypeRepository extends JpaRepository<ElectricVehicleType, String> {
+
+}

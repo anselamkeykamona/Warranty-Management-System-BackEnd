9df0c1ecffb70febbaae9d4d62481feeb53ac646
Tom <154430738+anselamkeykamona@users.noreply.github.com>
2025-10-11 11:46:15 +0700

    Revert "luồng đăng ký và phân quyền tạo tài khoản (#12)"
    This reverts commit 48ee3f064e15fe2db915d98ed045dd493d376585.

--- warrantyclaim-api/src/main/java/com/warrantyclaim/warrantyclaim_api/exception/AccountLockedException.java
+++
@@ -1,7 +0,0 @@
-package com.warrantyclaim.warrantyclaim_api.exception;
-
-public class AccountLockedException extends RuntimeException {
-    public AccountLockedException(String message) {
-        super(message);
-    }
-}

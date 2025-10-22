2b47d6db8974396f492c7cd7980759da91e5bbdf
Lê Minh Đức <154430738+anselamkeykamona@users.noreply.github.com>
2025-10-22 10:37:16 +0700

    Delete src directory

--- src/main/java/com/warrantyclaim/warrantyclaim_api/repository/ScStaffRepository.java
+++
@@ -1,9 +0,0 @@
-package com.warrantyclaim.warrantyclaim_api.repository;
-
-import com.warrantyclaim.warrantyclaim_api.entity.SCStaff;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScStaffRepository extends JpaRepository<SCStaff, String> {
-}

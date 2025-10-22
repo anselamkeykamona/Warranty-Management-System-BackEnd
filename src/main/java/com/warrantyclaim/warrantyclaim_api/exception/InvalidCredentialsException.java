2068a01f7ecbcdd02fcbf6fbb6bb707653e604b8
anselamkeykamona <sit96leminhduc@gmail.com>
2025-10-22 10:26:31 +0700

    Delete unecessary

--- src/main/java/com/warrantyclaim/warrantyclaim_api/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.warrantyclaim.warrantyclaim_api.exception;
-
-public class InvalidCredentialsException extends RuntimeException{
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
